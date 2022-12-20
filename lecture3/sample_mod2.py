d7f311b1d0b427686d0980aa7fc4048e924e7b52
tendo-sms <94833815+tendo-sms@users.noreply.github.com>
2022-12-20 18:56:08 +0900

    Delete sample_mod2.py

--- lecture3/sample_mod2.py
+++
@@ -1,5 +0,0 @@
-def print_string():
-    print("モジュール「sample_mod2」の関数「print_string」が呼び出されました。")
-
-string_var1 = "モジュール「sample_mod2」で定義された変数「string_var1」が参照されました。"
-string_var2 = "モジュール「sample_mod2」で定義された変数「string_var2」が参照されました。"
